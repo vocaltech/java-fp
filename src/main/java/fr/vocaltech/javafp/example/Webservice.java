ad0d2d01a95361fb2019379a4513d06a4f176817
vocaltech <it@vocaltech.fr>
2023-08-02 01:39:20 +0200

    Replace IOException with WebserviceException

--- src/main/java/fr/vocaltech/javafp/example/Webservice.java
+++ src/main/java/fr/vocaltech/javafp/example/Webservice.java
@@ -1,7 +1,5 @@
 package fr.vocaltech.javafp.example;
 
-import java.io.IOException;
-
 public interface Webservice {
-    boolean create(Doc doc) throws IOException;
+    boolean create(Doc doc) throws WebserviceException;
 }
