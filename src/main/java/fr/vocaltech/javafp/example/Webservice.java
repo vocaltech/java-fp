2a574735ad66e13de18d1f82b62795f55cb709c6
vocaltech <it@vocaltech.fr>
2023-08-26 06:08:20 +0200

    Update with CompletableFuture return

--- src/main/java/fr/vocaltech/javafp/example/Webservice.java
+++ src/main/java/fr/vocaltech/javafp/example/Webservice.java
@@ -1,5 +1,7 @@
 package fr.vocaltech.javafp.example;
 
+import java.util.concurrent.CompletableFuture;
+
 public interface Webservice {
-    boolean create(Doc doc) throws WebserviceException;
+    CompletableFuture<Resource> create(Doc doc) throws WebserviceException;
 }
