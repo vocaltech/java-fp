adff53aa3d0477869474304f27bbbebbb99e7e14
vocaltech <it@vocaltech.fr>
2023-08-27 06:42:53 +0200

    Update interface to be compatible with DocImmutableWith

--- src/main/java/fr/vocaltech/javafp/example/Webservice.java
+++ src/main/java/fr/vocaltech/javafp/example/Webservice.java
@@ -4,4 +4,5 @@ import java.util.concurrent.CompletableFuture;
 
 public interface Webservice {
     CompletableFuture<Resource> create(Doc doc) throws WebserviceException;
+    CompletableFuture<Resource> create(DocImmutableWith doc) throws WebserviceException;
 }
