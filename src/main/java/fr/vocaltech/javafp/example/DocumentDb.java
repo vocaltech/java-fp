27978e9b1adb43a1578cb2155cb6850e493866eb
vocaltech <it@vocaltech.fr>
2023-08-27 06:44:08 +0200

    Update interface to be compatible with DocImmutableWith

--- src/main/java/fr/vocaltech/javafp/example/DocumentDb.java
+++ src/main/java/fr/vocaltech/javafp/example/DocumentDb.java
@@ -2,4 +2,5 @@ package fr.vocaltech.javafp.example;
 
 public interface DocumentDb {
     boolean update(Doc doc) throws DocumentDbException;
+    boolean update(DocImmutableWith doc) throws DocumentDbException;
 }
