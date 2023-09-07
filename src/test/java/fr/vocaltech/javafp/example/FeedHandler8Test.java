cba4e252c9a0b470462c6977f940e4629a25ec0f
vocaltech <it@vocaltech.fr>
2023-09-07 22:59:56 +0200

    Add tests for functions with private visibility

--- src/test/java/fr/vocaltech/javafp/example/FeedHandler8Test.java
+++ src/test/java/fr/vocaltech/javafp/example/FeedHandler8Test.java
@@ -1,14 +1,21 @@
 package fr.vocaltech.javafp.example;
 
 import io.vavr.control.Try;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.platform.commons.util.ReflectionUtils;
+
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
+import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.List;
+
+import java.util.function.BiFunction;
 import java.util.function.Function;
+import java.util.function.Predicate;
 
 import static org.assertj.core.api.Assertions.*;
 
@@ -38,20 +45,70 @@ class FeedHandler8Test {
         changes.add(docImmutableWith);
     }
 
+    @SuppressWarnings("unchecked")
+    private Predicate<DocImmutableWith> getPredicateIsImportant() {
+        Predicate<DocImmutableWith> predicateIsImportant;
+
+        List<Field> fields = ReflectionUtils.findFields(FeedHandler8.class, field -> field.getName().equals("isImportant"), ReflectionUtils.HierarchyTraversalMode.TOP_DOWN);
+        Field fieldIsImportant = fields.get(0);
+        fieldIsImportant.setAccessible(true);
+
+        org.junit.platform.commons.function.Try<Object> tryField = ReflectionUtils.tryToReadFieldValue(fieldIsImportant);
+
+        try {
+             predicateIsImportant = (Predicate<DocImmutableWith>) tryField.get();
+        } catch (Exception e) {
+            throw new RuntimeException();
+        }
+
+        return predicateIsImportant;
+    }
+
+    @SuppressWarnings("unchecked")
+    private BiFunction<DocImmutableWith, Resource, DocImmutableWith> getBiFunctionSetToProcessed() {
+        BiFunction<DocImmutableWith, Resource, DocImmutableWith> biFunctionSetToProcessed;
+
+        List<Field> fields = ReflectionUtils.findFields(FeedHandler8.class, field -> field.getName().equals("setToProcessed"), ReflectionUtils.HierarchyTraversalMode.TOP_DOWN);
+        Field fieldSetToProcessed = fields.get(0);
+        fieldSetToProcessed.setAccessible(true);
+
+        org.junit.platform.commons.function.Try<Object> tryField = ReflectionUtils.tryToReadFieldValue(fieldSetToProcessed);
+
+        try {
+            biFunctionSetToProcessed = (BiFunction<DocImmutableWith, Resource, DocImmutableWith>) tryField.get();
+        } catch (Exception e) {
+            throw new RuntimeException();
+        }
+        return biFunctionSetToProcessed;
+    }
+
     @Test
     void givenDocImportant_whenTestPredicateIsImportant_thenTrue() {
         DocImmutableWith docImportant = new DocImmutableWith("GROOVY", "IMPORTANT", 0, "NEW", null);
 
-        assertThat(FeedHandler8.isImportant.test(docImportant)).isTrue();
+        Predicate<DocImmutableWith> isImportant = getPredicateIsImportant();
+        assertThat(isImportant.test(docImportant)).isTrue();
     }
-
     @Test
     void givenDocNotImportant_whenTestPredicateIsImportant_thenFalse() {
         DocImmutableWith docNotImportant = new DocImmutableWith("Rust", "MINOR", 0, "NEW", null);
 
-        assertThat(FeedHandler8.isImportant.test(docNotImportant)).isFalse();
+        Predicate<DocImmutableWith> isImportant = getPredicateIsImportant();
+        assertThat(isImportant.test(docNotImportant)).isFalse();
     }
 
+    @Test
+    void givenDocWithNewStatus_whenApplySetToProcessed_thenOk() {
+        DocImmutableWith docImportant = new DocImmutableWith("GROOVY", "IMPORTANT", 0, "NEW", null);
+        Resource resource = new Resource();
+        resource.put("id", 9);
+
+        BiFunction<DocImmutableWith, Resource, DocImmutableWith> setToProcessed = getBiFunctionSetToProcessed();
+        DocImmutableWith processedDoc = setToProcessed.apply(docImportant, resource);
+
+        assertThat(processedDoc.getStatus()).isEqualTo("PROCESSED");
+        assertThat(processedDoc.getApiId()).isEqualTo(9);
+    }
     @Test
     void givenChanges_whenFilterDocIsImportant_thenOk() {
         // GIVEN
@@ -66,5 +123,4 @@ class FeedHandler8Test {
         // THEN
         assertThat(processed.get(0).getType()).isEqualTo("IMPORTANT");
     }
-
 }
