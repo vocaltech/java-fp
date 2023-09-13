698f6c140aa2f1322cf456f42d6b7411346bb372
vocaltech <it@vocaltech.fr>
2023-09-14 00:57:56 +0200

    Implement handle() with DEFAULT_FILTER, DEFAULT_SUCCESS_MAPPER, DEFAULT_FAILURE_MAPPER

--- src/main/java/fr/vocaltech/javafp/example/FeedHandler9.java
+++ src/main/java/fr/vocaltech/javafp/example/FeedHandler9.java
@@ -9,6 +9,7 @@ import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 public class FeedHandler9 {
+    private final Repository repository;
     private static final Predicate<DocImmutableWith> DEFAULT_FILTER = doc -> doc.getType().equals("IMPORTANT");
     private static final BiFunction<DocImmutableWith, Resource, DocImmutableWith> DEFAULT_SUCCESS_MAPPER = (doc, resource) ->
             doc.withStatus("PROCESSED")
@@ -16,12 +17,15 @@ public class FeedHandler9 {
     private static final BiFunction<DocImmutableWith, Throwable, DocImmutableWith> DEFAULT_FAILURE_MAPPER = (doc, e) ->
             doc.withStatus("FAILED")
                     .withError(e.getMessage());
+
+    public FeedHandler9(Repository repository) {
+        this.repository = repository;
+    }
     public List<DocImmutableWith> handle(List<DocImmutableWith> changes,
                                          Function<DocImmutableWith, Try<Resource>> creator,
                                          Predicate<DocImmutableWith> filter,
                                          BiFunction<DocImmutableWith, Resource, DocImmutableWith> successMapper,
-                                         BiFunction<DocImmutableWith, Throwable, DocImmutableWith> failureMapper
-    ) {
+                                         BiFunction<DocImmutableWith, Throwable, DocImmutableWith> failureMapper) {
         return changes.stream()
                 .filter(filter)
                 .map(doc -> creator.apply(doc)
@@ -31,4 +35,10 @@ public class FeedHandler9 {
                 )
                 .collect(Collectors.toList());
     }
+
+    public List<DocImmutableWith> handle(List<DocImmutableWith> changes,
+                                         Function<DocImmutableWith, Try<Resource>> creator) {
+        return handle(changes, creator, DEFAULT_FILTER, DEFAULT_SUCCESS_MAPPER, DEFAULT_FAILURE_MAPPER);
+    }
+
 }
