3d06ba433f000c8b3e416b11e0fc8fb6ebbf79ab
vocaltech <it@vocaltech.fr>
2023-09-17 20:26:25 +0200

    Add Repository interface and some exceptions

--- src/main/java/fr/vocaltech/javafp/example/Resource.java
+++ src/main/java/fr/vocaltech/javafp/example/Resource.java
@@ -2,4 +2,19 @@ package fr.vocaltech.javafp.example;
 
 import java.util.HashMap;
 
-public class Resource extends HashMap {}
+public class Resource extends HashMap<String, Integer> {}
+
+interface Repository {
+    Resource findById(int id);
+}
+class DuplicateResourceException extends RuntimeException {
+    public DuplicateResourceException(String message) {
+        super(message);
+    }
+}
+
+class SpecialException extends RuntimeException {
+    public SpecialException(String message) {
+        super(message);
+    }
+}
